3de2b1cce41f521a4f7a2bb0623002fcb12e8986
demetrxx <dmytro.bielousov@bndigital.co>
2022-11-05 15:02:36 +0200

    hw5 done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { auth, validation, ctrlWrapper } = require('../../middlewares');
+const { auth, upload, validation, ctrlWrapper } = require('../../middlewares');
 const { joiLoginSchema, joiSignupSchema, joiChangeSubsSchema } = require('../../models/user');
 const { users: ctrl } = require('../../controllers');
 
@@ -13,6 +13,8 @@ router.post('/login', validation(joiLoginSchema), ctrlWrapper(ctrl.login));
 router.get('/logout', auth, ctrlWrapper(ctrl.logout));
 
 router.get('/current', auth, ctrlWrapper(ctrl.current));
+
+router.patch('/avatars', auth, upload.single('avatar'), ctrlWrapper(ctrl.updateAvatar));
 
 router.patch('/', validation(joiChangeSubsSchema), auth, ctrlWrapper(ctrl.changeSubs));
 
