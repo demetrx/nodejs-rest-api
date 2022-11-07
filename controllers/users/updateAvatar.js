3db5d19398a2e97adb9848618e86fe44648c3549
demetrxx <dmytro.bielousov@bndigital.co>
2022-11-07 11:52:21 +0200

    add jimp

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,19 +1,28 @@
 const { User } = require('../../models');
 const path = require("path");
 const fs = require('fs/promises');
+const Jimp = require('jimp');
 
 const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
 
 const updateAvatar = async (req, res) => {
-  const {path: tempUpload, originalname} = req.file;
-  const {_id: id} = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const { _id: id } = req.user;
   const imageName = `${id}_${originalname}`;
-  try{
+  try {
     const resultUpload = path.join(avatarsDir, imageName);
-    await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("public", 'avatars', imageName);
-    await User.findByIdAndUpdate(req.user._id, {avatarURL});
-    res.json({avatarURL})
+
+    Jimp.read(tempUpload, async (err, photo) => {
+      if (err) throw err;
+      photo
+        .resize(250, 250)
+        .write(tempUpload);
+
+      await fs.rename(tempUpload, resultUpload);
+      const avatarURL = path.join("public", 'avatars', imageName);
+      await User.findByIdAndUpdate(req.user._id, { avatarURL });
+      res.json({ avatarURL })
+    });
   } catch (err) {
     await fs.unlink(tempUpload);
     throw err;
